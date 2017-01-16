f7c67ce934b4054aa5602ef0ec1ce6b953ece644
Akshatha Channakrishnappa <akshathac91@gmail.com>
2017-01-15 23:14:27 -0600

    Update dbinfo.php
    Remove credentials

--- dbinfo.php
+++ dbinfo.php
@@ -5,9 +5,9 @@
  * Date: 1/14/2017
  * Time: 9:29 PM
  */
-$username='Aksh30';
-$password='Krishna30';
-$host = "mydbinstance.cmsecos5l0vz.us-east-1.rds.amazonaws.com:3306";
-$database = "WishList";
+$username=//username;
+$password=//password;
+$host =//host;
+$database = //databaseName;
 $dsn = "mysql:host=$host;dbname=$database";
 ?>
